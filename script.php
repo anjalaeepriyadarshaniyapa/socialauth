a47d8c1a138ec6f40fc8d6f5b2d0ec1a6ab204db
Anjalee Priyadarshani <55070469+anjalaeepriyadarshaniyapa@users.noreply.github.com>
2021-12-21 14:20:12 +0530

    Add files via upload
    Initial Commit

---
+++ script.php
@@ -0,0 +1,10 @@
+<?php
+
+$fb = new Facebook\Facebook([
+    'app_id' => '1087428342078288', //app ID UNIQUE 
+    'app_secret' => '      ',//app-secret
+    'default_graph_version' => 'v2.11',
+    ]);
+
+?>
+044e2f995b3674cb2326493de7bde5da
